bde7729730e28abb3dc3ce88342b99fc179c460f
70140461 <70140461@student.uol.edu.pk>
2024-10-29 21:58:51 -0700

    class

--- flutter lab/lab4/lib/currency_converter_page.dart
+++ flutter lab/lab4/lib/currency_converter_page.dart
@@ -1,85 +1,73 @@
 import 'package:flutter/material.dart';
-
-class CurrencyConverterPage extends StatelessWidget {
+class CurrencyConverterPage extends StatefulWidget {
   const CurrencyConverterPage({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
+  State<CurrencyConverterPage> createState() =>
+  _CurrencyConverterPageState();
+}
+
+class _CurrencyConverterPageState
+extends State<CurrencyConverterPage>{
+  double result =0;
+  TextEditingController textEditingController = TextEditingController();
+  @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 126, 108, 108),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children:  [
-          TextDisplay(),
-          CustomTextField(),
-          CustomTextButton(),
+        children: [
+           Text(
+            result.toString(),
+            style: const TextStyle(
+              fontSize: 22,
+            ),
+          ),
+           Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller:textEditingController,
+              decoration: const InputDecoration(
+                hintText: "please",
+                hintStyle: TextStyle(color: Color.fromARGB(255, 147, 143, 148)),
+                prefixIcon: Icon(
+                  Icons.monetization_on,
+                ),
+                filled: true,
+                fillColor: Color.fromARGB(255, 147, 143, 148),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  borderSide: BorderSide(color: Color.fromARGB(255, 175, 171, 175), width: 2.0, style: BorderStyle.solid),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  borderSide: BorderSide(color: Color.fromARGB(255, 149, 147, 150), width: 2.0, style: BorderStyle.solid),
+                ),
+              ),
+              keyboardType: TextInputType.numberWithOptions(
+                decimal: true),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                result = double.parse(textEditingController.text) * 300;
+              });
+              // debugPrint('pressed');
+            },
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll<Color>(Color.fromARGB(255, 148, 147, 158)),
+              foregroundColor: WidgetStatePropertyAll<Color>(Color.fromARGB(255, 148, 147, 158)),
+            ),
+            child: const Text(
+              'convert',
+              style:TextStyle(color:Colors.black),
+            )
+          )
         ],
       ),
     );
-  }
-}
-class TextDisplay extends StatelessWidget {
-  const TextDisplay({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Text(
-      '0',
-      style: TextStyle(
-        fontSize: 22,
-      ),
-    );
   }
 }
 
-class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: "please",
-          hintStyle: TextStyle(color: Colors.grey),
-          prefixIcon: Icon(
-            Icons.monetization_on,
-          ),
-          filled: true,
-          fillColor: Colors.grey,
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(50)),
-            borderSide: BorderSide(color: Colors.grey,width: 2.0,style: BorderStyle.solid),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(50)),
-            borderSide: BorderSide(color: Colors.grey,width: 2.0,style: BorderStyle.solid),
-          ),
-        ),
-        keyboardType: TextInputType.numberWithOptions(decimal: true, signed: false),
-      ),
-    );
-  }
-}
-
-class CustomTextButton extends StatelessWidget {
-  const CustomTextButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        debugPrint('pressed');
-      },
-      style: const ButtonStyle(
-        backgroundColor:  WidgetStatePropertyAll<Color>(Colors.grey),
-        foregroundColor: WidgetStatePropertyAll<Color>(Colors.grey),
-        fixedSize: WidgetStatePropertyAll<Size>(Size(100, 50)),
-      ),
-      child: const Text(
-        'convert',
-      ),
-    );
-  }
-}
